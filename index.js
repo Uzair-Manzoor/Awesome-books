964fef085ea49c6bb7dcb8396c511a26947e3942
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-07-11 11:37:30 +0500

    fix linter errors

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-let books = [];
+const books = [];
 
+function updateBookList() {
+  const bookList = document.getElementById('booksList');
+
+  bookList.innerHTML = '';
+  books.forEach((book, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${book.title} by ${book.author}`;
+
+    function removeBook(index) {
+      books.splice(index, 1);
+      updateBookList();
+    }
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => removeBook(index));
+
+    li.appendChild(removeBtn);
+    bookList.appendChild(li);
+  });
+}
 
 function addBook(event) {
   event.preventDefault();
 
-  const titleInput = document.getElementById("title");
-  const authorInput = document.getElementById("author");
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
   const title = titleInput.value;
   const author = authorInput.value;
 
@@ -13,42 +34,11 @@ function addBook(event) {
 
   books.push(book);
 
-  titleInput.value = "";
-  authorInput.value = "";
+  titleInput.value = '';
+  authorInput.value = '';
 
-  
   updateBookList();
 }
 
-
-function removeBook(index) {
- 
-  books.splice(index, 1);
-
- 
-  updateBookList();
-}
-
-
-function updateBookList() {
-  const bookList = document.getElementById("booksList");
-
-  
-  bookList.innerHTML = "";
-
-  
-  books.forEach((book, index) => {
-    const li = document.createElement("li");
-    li.textContent = `${book.title} by ${book.author}`;
-
-    const removeBtn = document.createElement("button");
-    removeBtn.textContent = "   Remove";
-    removeBtn.addEventListener("click", () => removeBook(index));
-
-    li.appendChild(removeBtn);
-    bookList.appendChild(li);
-  });
-}
-
-const bookForm = document.getElementById("bookForm");
-bookForm.addEventListener("submit", addBook);
+const bookForm = document.getElementById('bookForm');
+bookForm.addEventListener('submit', addBook);
